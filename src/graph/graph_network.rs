a9d4bdea27749b7ad96cd4e5f2a70a84dc696e10
Ricky <ricky@rpgrafx.com>
2024-06-08 21:43:41 -0700

    Init commit

---
+++ src/graph/graph_network.rs
@@ -0,0 +1,21 @@
+mod graph {
+    struct Node {
+        pub activation_function: Box<dyn Fn(f64) -> f64>,
+        pub threshold : f64
+    }
+
+    struct Layer {
+        pub layer_size : u16,
+        pub nodes : Vec<Node>,
+        pub biases : Vec<f64>
+    }
+
+    fn activate(node: &Node, input: Vec<f64>, weights:Vec<f64>, bias: f64) -> f64 {
+        let mut sum : f64 = 0.0;
+        for i in 0..input.len() {
+            sum += input[i] * weights[i];
+        }
+        sum += bias;
+        (*(*node).activation_function)(sum)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+mod graph;
+fn main() {
+    println!("Hello, world!");
+}
