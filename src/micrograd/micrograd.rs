dd75844554783d2b183a4d71561be260e16b72de
Ricky <ricky@rpgrafx.com>
2024-06-12 20:32:40 -0700

    Test Gradient

--- src/micrograd/micrograd.rs
+++ src/micrograd/micrograd.rs
@@ -1,13 +1,26 @@
 mod micrograd {
+    use std::rc::Weak;
+
     pub struct Value {
-        pub data: f64
+        pub data: f64,
+        pub grad: f64,
+
+        pub backwards : fn(Weak<Value>, Weak<Value>),
+        pub previous : std::vec::Vec<Weak<Value>>
     }
 
     impl Value {
         pub fn add(&self, other: &Value) -> Value {
+            unsafe {  
+            let previous =  vec![Weak::from_raw(self), Weak::from_raw(other)];
             return Value {
-                data: self.data + other.data
-            }
+                data: self.data + other.data,
+                grad: 0.0,
+                backwards: |self_ref: Weak<Value>, other_ref: Weak<Value>|  {
+                    self_ref.upgrade().unwrap().grad += other_ref.upgrade().unwrap().grad;
+                },
+                previous
+            }}
         }
 
         pub fn mul(&self, other: &Value) -> Value {
@@ -18,6 +31,14 @@ mod micrograd {
 
         pub fn repr(&self) -> String {
             return format!("Value({})", self.data);
+        }
+    }
+
+    impl Iterator for Value {
+        type Item = Value;
+    
+        fn next(&mut self) -> Option<Self::Item> {
+            todo!()
         }
     }
 }
