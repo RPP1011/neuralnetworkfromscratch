8eab54fe607d5c2c3e26dfca8dddc2655f9acef6
Ricky <ricky@rpgrafx.com>
2024-06-13 23:18:18 -0700

    Fixed Type Errors Part 1

--- src/micrograd/micrograd.rs
+++ src/micrograd/micrograd.rs
@@ -1,4 +1,5 @@
 mod micrograd {
+    use std::borrow::BorrowMut;
     use std::cell::RefCell;
     use std::rc::{Rc, Weak};
     pub enum Operation {
@@ -6,68 +7,63 @@ mod micrograd {
         MUL,
         POW,
     }
+    
+
     pub struct Value {
         pub data: f64,
         pub grad: f64,
 
         pub backwards: Option<Box<dyn Fn()>>,
-        pub previous: Vec<Weak<RefCell<Value>>>,
+        pub previous: Vec<Rc<RefCell<Value>>>,
         pub op: Operation,
     }
 
-    impl Value {
-        pub fn add(&self, other: &Rc<RefCell<Self>>) -> Rc<RefCell<Value>> {
-            let previous = vec![
-                Rc::downgrade(&Rc::new(RefCell::new(self.clone()))),
-                Rc::downgrade(&Rc::new(other.borrow())),
+        pub fn add(this: Rc<RefCell<Value>>, other: Rc<RefCell<Value>>) -> Rc<RefCell<Value>> {
+            let previous: Vec<Rc<RefCell<Value>>> = vec![
+                this.clone(),
+                other.clone(),
             ];
-            let out = Rc::new(RefCell::new(Value {
-                data: self.data + other.borrow().data,
+            let mut out = Value {
+                data: this.borrow().data + other.borrow().data,
                 grad: 0.0,
                 backwards: None,
                 previous,
                 op: Operation::ADD,
-            }));
+            };
 
-            let out_clone = Rc::clone(&out);
-            let self_clone = Rc::downgrade(&Rc::new(RefCell::new(self.clone())));
-            let other_clone = Rc::downgrade(other);
+            let out_reference = Rc::new(RefCell::new(out));
+
 
             out.borrow_mut().backwards = Some(Box::new(move || {
-                let out = out_clone.borrow();
-                if let Some(self_strong) = self_clone.upgrade() {
-                    self_strong.borrow_mut().grad += out.grad;
-                }
-                if let Some(other_strong) = other_clone.upgrade() {
-                    other_strong.borrow_mut().grad += out.grad;
-                }
+                let out_grad: f64 = out_reference.borrow().grad;
+                let mut this_instance =  this.borrow().borrow_mut();
+               
+                this_instance.grad += out_grad;
+                
             }));
 
-            out
+            out_reference
         }
 
-        pub fn mul(&self, other: &Value) -> Value {
-            unsafe {
-                let previous = vec![Weak::from_raw(self), Weak::from_raw(other)];
-                let mut out = Value {
-                    data: self.data * other.data,
-                    grad: 0.0,
-                    backwards: None,
-                    previous,
-                    op: Operation::MUL,
-                };
+        // pub fn mul(&self, other: &Value) -> Value {
+        //     unsafe {
+        //         let previous = vec![Weak::from_raw(self), Weak::from_raw(other)];
+        //         let mut out = Value {
+        //             data: self.data * other.data,
+        //             grad: 0.0,
+        //             backwards: None,
+        //             previous,
+        //             op: Operation::MUL,
+        //         };
 
-                out.backwards = Some(Box::new(move || -> () {
-                    self.grad *= other.grad * out.grad;
-                    other.grad *= self.grad * out.grad;
-                }));
+        //         out.backwards = Some(Box::new(move || -> () {
+        //             self.grad *= other.grad * out.grad;
+        //             other.grad *= self.grad * out.grad;
+        //         }));
 
-                return out;
-            }
-        }
+        //         return out;
+        //     }
+        // }
 
-        pub fn repr(&self) -> String {
-            return format!("Value({})", self.data);
-        }
-    }
+    
 }
