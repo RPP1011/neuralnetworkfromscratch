28b50c1f5fffccbc5da9881abf27446865362add
Ricky <ricky@rpgrafx.com>
2024-06-17 21:31:56 -0700

    Fixed Forward

--- src/micrograd/micrograd.rs
+++ src/micrograd/micrograd.rs
@@ -1,5 +1,6 @@
 mod micrograd {
-    use std::borrow::BorrowMut;
+    use std::borrow::{Borrow, BorrowMut};
+    use std::cell::RefCell;
     use std::rc::{Rc};
     
     #[derive(Clone)]
@@ -10,30 +11,39 @@ mod micrograd {
     }
 
     impl Operation {
-        pub fn forward(&self, a: Rc<Value>, b: Rc<Value>) -> f64 {
+        pub fn forward(&self, a: Rc<RefCell<Value>>, b: Rc<RefCell<Value>>) -> f64 {
+            let a_reference : &RefCell<Value> = a.borrow();
+            let a_data = a_reference.borrow().data;
+            
+            let b_reference : &RefCell<Value> = b.borrow();
+            let b_data = b_reference.borrow().data;
+            
             match self {
-                Operation::ADD => a.clone().data + b.clone().data ,
-                Operation::MUL => a.clone().data * b.clone().data,
-                Operation::POW => a.clone().data.powf(b.clone().data),
+                Operation::ADD => a_data + b_data,
+                Operation::MUL => a_data * b_data,
+                Operation::POW => a_data.powf(b_data),
             }
         }
 
-        pub fn backwards(&self, a: Rc<Value>, b: Rc<Value>, out: Rc<Value>) {
-            match self {
-                Operation::ADD => {
-                    a.clone().borrow_mut().grad += out.clone().grad;
-                    b.clone().borrow_mut().grad += out.clone().grad;
-                },
-                Operation::MUL => {
-                    a.clone().borrow_mut().grad += b.clone().data * out.clone().grad;
-                    b.clone().borrow_mut().grad += a.clone().data * out.clone().grad;
-                },
-                Operation::POW => {
-                    a.clone().borrow_mut().grad += b.clone().data * a.clone().data.powf(b.clone().data - 1.0) * out.clone().grad;
-                    b.clone().borrow_mut().grad += a.clone().data.powf(b.clone().data) * out.clone().grad * (a.clone().data.ln());
-                },
-            }
-        }
+        // pub fn backwards(&self, a: Rc<RefCell<Value>>, b: Rc<RefCell<Value>>, out: Rc<RefCell<Value>>) {
+        //     let mut a = a.borrow().borrow_mut();
+        //     let mut b = b.borrow().borrow_mut();
+        //     let out = out.borrow().borrow();
+        //     match self {
+        //         Operation::ADD => {
+        //             a.grad += out.clone().grad;
+        //             b.grad += out.clone().grad;
+        //         },
+        //         Operation::MUL => {
+        //             a.clone().borrow_mut().grad += b.clone().data * out.clone().grad;
+        //             b.clone().borrow_mut().grad += a.clone().data * out.clone().grad;
+        //         },
+        //         Operation::POW => {
+        //             a.clone().borrow_mut().grad += b.clone().data * a.clone().data.powf(b.clone().data - 1.0) * out.clone().grad;
+        //             b.clone().borrow_mut().grad += a.clone().data.powf(b.clone().data) * out.clone().grad * (a.clone().data.ln());
+        //         },
+        //     }
+        // }
     }
 
     pub enum CreateValueOption {
@@ -42,26 +52,26 @@ mod micrograd {
     }
    
     pub struct Graph {
-        pub values: Vec<Rc<Value>>,
+        pub values: Vec<Rc<RefCell<Value>>>,
         pub layers: Vec<Layer>
     }
 
     impl Graph {
-        pub fn add_value(&mut self, value: CreateValueOption) -> Rc<Value>{
-            let out = Rc::new(Value {
+        pub fn add_value(&mut self, value: CreateValueOption) -> Rc<RefCell<Value>>{
+            let out = Rc::new(RefCell::new(Value {
                 data: 0.0,
                 grad: 0.0,
                 index: self.values.len(),
                 previous: vec![],
                 op: Operation::ADD,
-            });
+            }));
 
             match value {
                 CreateValueOption::EMPTY => {
                     self.values.push(out.clone());
                 },
                 CreateValueOption::DATA(value) => {
-                    self.values.push(Rc::new(value));
+                    self.values.push(Rc::new(RefCell::new(value)));
                 }          
             };
             
@@ -98,45 +108,22 @@ mod micrograd {
         pub op: Operation,
     }
 
-    pub fn add(graph: Rc<Graph>, a:Rc<Value>, b:Rc<Value>) -> Rc<Value> {
-        let mut graph_reference = graph.clone();
+    // pub fn add(graph: Rc<RefCell<Graph>>, a:Rc<RefCell<Value>>, b:Rc<RefCell<Value>>) -> Rc<RefCell<Value>> {
+    //     let graph_reference: Rc<RefCell<Graph>> = graph.clone();
 
-        let out = Value {
-            data: graph_reference.values[a.index].data + graph_reference.values[b.index].data,
-            grad: 0.0,
-            index: graph_reference.values.len(),
-            previous: vec![a.index, b.index],
-            op: Operation::ADD,
-        };
+    //     let values: Vec<Rc<RefCell<Value>>> = graph_reference.borrow().values;
+    //     let a_index = a.borrow().index;
+    //     let b_index = b.borrow().index;
 
-        graph_reference.borrow_mut().add_value(CreateValueOption::DATA(out.clone()))
-    }
+    //     let out = Value {
+    //         data: values[a_index].borrow().data + values[b_index].borrow().data,
+    //         grad: 0.0,
+    //         index: graph_reference.borrow().values.len(),
+    //         previous: vec![a_index, b_index],
+    //         op: Operation::ADD,
+    //     };
 
-    pub fn mul(graph: Rc<Graph>, a:Rc<Value>, b:Rc<Value>) -> Rc<Value> {
-        let mut graph_reference = graph.clone();
-
-        let out = Value {
-            data: graph_reference.values[a.index].data + graph_reference.values[b.index].data,
-            grad: 0.0,
-            index: graph_reference.values.len(),
-            previous: vec![a.index, b.index],
-            op: Operation::MUL,
-        };
-
-        graph_reference.borrow_mut().add_value(CreateValueOption::DATA(out.clone()))
-    }
-
-    pub fn pow(graph: Rc<Graph>, a:Rc<Value>, b:Rc<Value>) -> Rc<Value> {
-        let mut graph_reference = graph.clone();
-
-        let out = Value {
-            data: graph_reference.values[a.index].data + graph_reference.values[b.index].data,
-            grad: 0.0,
-            index: graph_reference.values.len(),
-            previous: vec![a.index, b.index],
-            op: Operation::POW,
-        };
-
-        graph_reference.borrow_mut().add_value(CreateValueOption::DATA(out.clone()))
-    }
+    //     let mut mut_graph_reference = graph.clone().borrow().borrow_mut();
+    //     mut_graph_reference.add_value(CreateValueOption::DATA(out.clone()))
+    // }
 }
