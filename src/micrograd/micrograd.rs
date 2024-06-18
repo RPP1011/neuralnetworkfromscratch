46afa83499ff8b3bcc8d1c876f2768d43b4688fb
Ricky <ricky@rpgrafx.com>
2024-06-17 21:59:39 -0700

    Fixed Compilation Issues

--- src/micrograd/micrograd.rs
+++ src/micrograd/micrograd.rs
@@ -108,22 +108,26 @@ mod micrograd {
         pub op: Operation,
     }
 
-    // pub fn add(graph: Rc<RefCell<Graph>>, a:Rc<RefCell<Value>>, b:Rc<RefCell<Value>>) -> Rc<RefCell<Value>> {
-    //     let graph_reference: Rc<RefCell<Graph>> = graph.clone();
+    pub fn add(graph: Rc<RefCell<Graph>>, a:Rc<RefCell<Value>>, b:Rc<RefCell<Value>>) -> Rc<RefCell<Value>> {
+        
+        let values: &Vec<Rc<RefCell<Value>>> = &graph.as_ref().borrow().values;
+        let a_index = a.as_ref().borrow().index;
+        let b_index = b.as_ref().borrow().index;
 
-    //     let values: Vec<Rc<RefCell<Value>>> = graph_reference.borrow().values;
-    //     let a_index = a.borrow().index;
-    //     let b_index = b.borrow().index;
+        let a_data = a.as_ref().borrow().data;
+        let b_data = b.as_ref().borrow().data;
 
-    //     let out = Value {
-    //         data: values[a_index].borrow().data + values[b_index].borrow().data,
-    //         grad: 0.0,
-    //         index: graph_reference.borrow().values.len(),
-    //         previous: vec![a_index, b_index],
-    //         op: Operation::ADD,
-    //     };
+        let out = Value {
+            data: a_data + b_data,
+            grad: 0.0,
+            index: values.len(),
+            previous: vec![a_index, b_index],
+            op: Operation::ADD,
+        };
 
-    //     let mut mut_graph_reference = graph.clone().borrow().borrow_mut();
-    //     mut_graph_reference.add_value(CreateValueOption::DATA(out.clone()))
-    // }
+        let graph_reference: Rc<RefCell<Graph>> = graph.clone();
+
+        let mut mut_graph_reference = graph_reference.as_ref().borrow_mut();
+        mut_graph_reference.add_value(CreateValueOption::DATA(out.clone()))
+    }
 }
