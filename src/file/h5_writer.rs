8228fb84257c78dbfc9bd2f0b5efe99a602211d3
Ricky <ricky@rpgrafx.com>
2024-06-29 22:34:08 -0700

    Make the code compile

--- src/file/h5_writer.rs
+++ src/file/h5_writer.rs
@@ -5,9 +5,9 @@ use serde_json; // Add this line to import serde_json crate
 
 
 pub fn save_model(model: &dyn Model, path: &str) -> Result{
-    let file = File::create(path)?;
-    let mut writer = BufWriter::new(file);
-    let mut encoder = serde_json::Serializer::new(&mut writer);
+    let file = File::create(path);
+    // let mut writer = BufWriter::new(file);
+    // let mut encoder = serde_json::Serializer::new(&mut writer);
     // model.serialize(&mut encoder)?;
     Ok(())
 }
